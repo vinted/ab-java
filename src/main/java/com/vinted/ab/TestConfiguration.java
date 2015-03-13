a98def4239e63e9cd5303dbca440e3a72d509c72
Saulius Grigaliunas <saulius@ninja.lt>
2015-03-13 13:33:31 +0200

    Add getAllTestVariantsForTest method to TestConfiguration
    Allows getting all test variants even for tests that reached their
    end_at date.

--- src/main/java/com/vinted/ab/TestConfiguration.java
+++ src/main/java/com/vinted/ab/TestConfiguration.java
@@ -32,8 +32,7 @@ public class TestConfiguration {
      * @return List of running tests assigned to user
      */
     public List<AbTest> getRunningTests(String identifier) {
-        String hex = DigestUtils.sha256Hex(salt + identifier);
-        BigInteger bucketId = new BigInteger(hex, 16).mod(BigInteger.valueOf(bucketCount));
+        BigInteger bucketId = getAssignedBucketId(identifier);
 
         List<AbTest> tests = new ArrayList<>();
         for (AbTest test : abTests) {
@@ -66,5 +65,57 @@ public class TestConfiguration {
         }
 
         return result;
+    }
+
+    /**
+     * @param identifier user identifier
+     * @return List of all (running + ended) tests assigned to user
+     */
+    public List<AbTest> getAllTests(String identifier) {
+        BigInteger bucketId = getAssignedBucketId(identifier);
+
+        List<AbTest> tests = new ArrayList<>();
+        for (AbTest test : abTests) {
+            if ((test.isAllBuckets() || test.getBuckets().contains(bucketId.intValue()))) {
+                tests.add(test);
+            }
+        }
+
+        return tests;
+    }
+
+    /**
+     * Return all variants for given test name if any.
+     * Will check among all running AND ended tests for given identifier with this test name.
+     *
+     * @param identifier user identifier
+     * @param testName   test name
+     * @return List of {@link com.vinted.ab.AbTestVariant} if has any. Empty list otherwise.
+     */
+    public List<AbTestVariant> getAllTestVariantsForTest(String identifier, String testName) {
+        List<AbTestVariant> result = new ArrayList<>();
+
+        for (AbTest test : getAllTests(identifier)) {
+            if (test.getName().equals(testName)) {
+                AbTestVariant variant = test.getAssignedVariant(identifier);
+                if (variant != null) {
+                    result.add(variant);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Return bucket ID to which the user is assigned.
+     *
+     * @param identifier user identifier
+     * @return Bucket ID as String.
+     */
+    private BigInteger getAssignedBucketId(String identifier) {
+        String hex = DigestUtils.sha256Hex(salt + identifier);
+
+        return new BigInteger(hex, 16).mod(BigInteger.valueOf(bucketCount));
     }
 }
